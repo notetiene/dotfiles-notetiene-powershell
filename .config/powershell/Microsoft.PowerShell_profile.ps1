0cfdbc18ae6f72083e52b49f4ad58b862cc3484c
Etienne Prud’homme <e.e.f.prudhomme@gmail.com>
2025-01-23 16:02:52 -0500

    Add PowerShell aliases

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,13 @@
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineOption -ShowToolTips
+
+# Reload the shell
+Set-Alias reload Reload-Powershell
+
+# http://xkcd.com/530/
+Set-Alias mute Set-SoundMute
+Set-Alias unmute Set-SoundUnmute
+
+# Update installed Ruby Gems, NPM, and their installed packages.
+Set-Alias update System-Update
